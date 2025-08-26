56a2f7e591dcfb0a32ebc60542aad5b6923b9773
Guuroh Christina <131835068+guurohchristina@users.noreply.github.com>
2025-08-26 12:34:18 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from app import app  # import the app variable from app/__init__.py
+from myapp import app  # import the app variable from app/__init__.py
 
 if __name__ == "__main__":
+
     app.run(debug=True) 
